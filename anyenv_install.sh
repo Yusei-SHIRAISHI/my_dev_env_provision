38abe1b58ff3f500d14e59259b56d1b75edaf77d
Yusei-SHIRAISHI <yusei.shiraishi29@gmail.com>
2024-02-27 00:43:22 +0900

    update

--- anyenv_install.sh
+++ anyenv_install.sh
@@ -16,5 +16,8 @@ git clone https://github.com/anyenv/anyenv ~/.anyenv
 
 if [ ! -e ~/.zsh.d/anyenv ]; then
   echo 'export PATH=$PATH:$HOME/.anyenv/bin' >> ~/.zsh.d/anyenv
-  echo 'eval "$(anyenv init -)"' >> ~/.zsh.d/anyenv
+  echo 'eval "$(anyenv init - zsh)"' >> ~/.zsh.d/anyenv
 fi
+
+mkdir -p $HOME/.anyenv/plugins
+git clone https://github.com/znz/anyenv-update.git $HOME/.anyenv/plugins/anyenv-update
